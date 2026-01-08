ac73ec7e5444334b4ab3c0aa67b582b98da2a45c
Roman <m3f3dr0nbristleback@gmail.com>
2026-01-08 04:59:52 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import { useEffect, useState } from "react";
-import Snowfall from "react-snowfall";
 
 function App() {
-  const [color] = useState({
-    dark: "#111111",
-    accent: "#222222"
-  });
-
-  const [btcPrice, setBtcPrice] = useState<string>("...");
-  const [colorText] = useState("#FFFFFF");
+  const [btcPrice, setBtcPrice] = useState("loading...");
 
   useEffect(() => {
     const fetchPrice = async () => {
       try {
-        const res = await fetch("https://ba2d64ef6772.ngrok-free.app/price");
+        const res = await fetch("https://main-crypto.onrender.com/price");
         const data = await res.json();
-        console.log("Fetched BTC price:", data.btcPrice); 
         setBtcPrice(data.btcPrice);
       } catch (err) {
         console.error("Error fetching BTC price:", err);
@@ -24,14 +16,13 @@ function App() {
 
     fetchPrice();
     const interval = setInterval(fetchPrice, 2000); 
-
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div style={{ background: `linear-gradient(120deg, ${color.dark}, ${color.accent})`, height: '100vh', padding: '20px' }}>
-      <h2 style={{ color: colorText, textAlign: "center" }}>{btcPrice}</h2>
-      <Snowfall />
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>BTC Price</h1>
+      <p>{btcPrice}</p>
     </div>
   );
 }
